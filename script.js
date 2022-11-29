ea1f42e3b1c566098a97fd1c4620ecedc36590d7
carlitoune873 <carlelieak@gmail.com>
2022-11-29 22:46:52 +0200

    login pop-up

--- script.js
+++ script.js
@@ -2,3 +2,13 @@ function show()
 {
     document.getElementById('sidebar').classList.toggle('active');
 }
+
+function POP()
+{
+    document.getElementById('PopUp').classList.toggle('active');
+}
+
+function UnPOP()
+{
+    document.getElementById('PopUp').classList.toggle('active');
+}
